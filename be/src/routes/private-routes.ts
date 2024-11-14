9ac3a47b279e93244e46f06ed6f8926f5c6c92b7
Tonysw2 <anthonyrmataa@gmail.com>
2024-11-14 12:07:33 -0300

    feat: add create, update and delete routes

--- be/src/routes/private-routes.ts
+++ be/src/routes/private-routes.ts
@@ -34,4 +34,60 @@ export async function privateRoutes(app: FastifyInstance) {
 
     return reply.code(200).send({ posts });
   });
+
+  app.post("/posts", async (request, reply) => {
+    const bodySchema = z.object({
+      title: z.string().min(1).max(100),
+      content: z.string().min(1).max(1000),
+    });
+
+    const { title, content } = bodySchema.parse(request.body);
+
+    const userId = request.user.sub;
+
+    await db("POSTS").insert({
+      TITLE: title,
+      CONTENT: content,
+      AUTHOR_ID: userId,
+    });
+
+    return reply.code(201).send();
+  });
+
+  app.put("/posts/:id", async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string(),
+    });
+
+    const bodySchema = z.object({
+      title: z.string().min(1).max(100).optional(),
+      content: z.string().min(1).max(1000).optional(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const { title, content } = bodySchema.parse(request.body);
+
+    const userId = request.user.sub;
+
+    await db("POSTS")
+      .update({ TITLE: title, CONTENT: content })
+      .where({ AUTHOR_ID: userId, ID: id });
+
+    return reply.code(204).send();
+  });
+
+  app.delete("/posts/:id", async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const userId = request.user.sub;
+
+    await db("POSTS").delete().where({ ID: id, AUTHOR_ID: userId });
+
+    return reply.code(204).send();
+  });
 }
